911ebf79fc6521c71227f25f92c8e73e8f940c22
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-06-26 00:38:01 +0530

    Add settings page.tsx

--- components/landing/AuthButtons.tsx
+++ components/landing/AuthButtons.tsx
@@ -21,6 +21,7 @@ import {
   User,
   LogOut,
   ChevronDown,
+  Settings,
 } from "lucide-react";
 
 export default function AuthButtons() {
@@ -175,6 +176,13 @@ export default function AuthButtons() {
 
           </DropdownMenuItem>
 
+        </Link>
+        
+        <Link href="/settings">
+            <DropdownMenuItem>
+              <Settings className="mr-2 h-4 w-4" />
+              Settings
+            </DropdownMenuItem>
         </Link>
 
         <DropdownMenuSeparator />
