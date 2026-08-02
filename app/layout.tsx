37e86a40068a01cd09aa48d105b964f368bb459d
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-08-03 02:24:35 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import { Inter, Sora } from "next/font/google";
 import "./globals.css";
 
 import AuthProvider from "@/components/providers/AuthProvider";
+
 import { Toaster } from "sonner";
+
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,11 +34,19 @@ export default function RootLayout({
       className={`${inter.variable} ${sora.variable} dark`}
     >
       <body className="font-sans antialiased">
+
         <AuthProvider>
           {children}
         </AuthProvider>
 
-        <Toaster richColors position="top-right" />
+        <Toaster
+          richColors
+          position="top-right"
+        />
+
+        <Analytics />
+        <SpeedInsights />
+
       </body>
     </html>
   );
